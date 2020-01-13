eb075e4dba8495c1f48d8bcd392b503b0a990f49
Erik Hedenström <erik@hedenstroem.com>
2020-01-13 17:15:11 +0100

    Improved caching

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,13 +1,27 @@
 const assets = ['/index.html', '/manifest.json', '/build/bundle.css', '/build/bundle.js'];
+const cacheName = 'svelte-app';
 
 self.addEventListener('install', event => {
+  // Pre-fetch core files
   event.waitUntil(caches.open('svelte-app').then(cache => cache.addAll(assets)));
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+    caches.open(cacheName).then(cache => {
+      return cache.match(event.request).then(function(response) {
+        return (
+          response ||
+          fetch(event.request).then(response => {
+            const url = new URL(event.request.url);
+            if (url.origin == location.origin) {
+              // Cache on demand from same origin
+              cache.put(event.request, response.clone());
+            }
+            return response;
+          })
+        );
+      });
     })
   );
 });
